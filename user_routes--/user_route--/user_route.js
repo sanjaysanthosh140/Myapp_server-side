487c742ec27c3ac840234294ffcf378eeb62adfb
sanjaysanthosh140 <sanjaysanthosh140@gmail.com>
2025-10-05 19:17:02 +0530

    forgot to run tsc

--- user_routes--/user_route--/user_route.js
+++ user_routes--/user_route--/user_route.js
@@ -21,8 +21,8 @@ router
     prompt: "consent",
 }));
 router.route("/oauth2/redirect/google").get(passport.authenticate("google", {
-    successRedirect: "https://saastoola-b3f60.web.app",
-    failureRedirect: "https://saastoola-b3f60.web.app/signup",
+    successRedirect: "https://client-side-24a22.web.app",
+    failureRedirect: "https://client-side-24a22.web.app/signup",
     session: true,
     failureMessage: true,
 }));
@@ -42,9 +42,9 @@ router
     access_type: "online",
 }));
 router.route("/oauth3/github/callback").get(passport.authenticate("github", {
-    successRedirect: "https://saastoola-b3f60.web.app",
+    successRedirect: "https://client-side-24a22.web.app",
     session: true,
-    failureRedirect: "https://saastoola-b3f60.web.app/signup",
+    failureRedirect: "https://client-side-24a22.web.app/signup",
     failureMessage: true,
 }));
 // router.get("/products", async (req: Request, res: Response) => {
